7145942e13eb8796c5e00707c5a979af525b6fef
Matt <matt_bristow@rocketmail.com>
2019-12-08 16:25:03 +0000

    added RegisterTypeFromScene extension

--- Runtime/Extensions.cs
+++ Runtime/Extensions.cs
@@ -17,5 +17,8 @@ namespace Autofac.Unity
 
         public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> RegisterTypeFromGameObject<T>(this ContainerBuilder builder) where T : Object =>
             builder.Register(x => x.ResolveFromGameObject<T>());
+
+        public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> RegisterTypeFromScene<T>(this ContainerBuilder builder) where T : Object =>
+           builder.Register(x => Object.FindObjectOfType<T>());
     }
 }
