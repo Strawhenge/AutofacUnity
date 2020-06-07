a136e9cfe751954444f2389682911d9438cd91f1
Matt <matt_bristow@rocketmail.com>
2020-06-07 20:51:50 +0100

    added config builder to property injector

--- Runtime/AutofacUnity.cs
+++ Runtime/AutofacUnity.cs
@@ -16,18 +16,23 @@ namespace Autofac.Unity
         public static void InjectUnsetPropertiesForGameObject(GameObject gameObject, params Parameter[] parameters) =>
             InjectUnsetPropertiesForGameObject(gameObject, parameters as IEnumerable<Parameter>);
 
-        public static void InjectUnsetPropertiesForGameObject(GameObject gameObject, IEnumerable<Parameter> parameters)
+        public static void InjectUnsetPropertiesForGameObject(GameObject gameObject, IEnumerable<Parameter> parameters) =>
+            InjectUnsetPropertiesForGameObject(gameObject, _ => { }, parameters);
+
+        public static void InjectUnsetPropertiesForGameObject(GameObject gameObject, Action<ContainerBuilder> configurationAction) =>
+         InjectUnsetPropertiesForGameObject(gameObject, configurationAction, Enumerable.Empty<Parameter>());
+
+        public static void InjectUnsetPropertiesForGameObject(GameObject gameObject, Action<ContainerBuilder> configurationAction, params Parameter[] parameters) =>
+            InjectUnsetPropertiesForGameObject(gameObject, configurationAction, parameters as IEnumerable<Parameter>);
+
+        public static void InjectUnsetPropertiesForGameObject(GameObject gameObject, Action<ContainerBuilder> configurationAction, IEnumerable<Parameter> parameters)
         {
-            var scope = CreateScopeForGameObject(gameObject);
+            var scope = GameObjectScopes.Create(gameObject, configurationAction);
 
             foreach (var monoBehaviour in gameObject.GetComponentsInChildren<MonoBehaviour>())
             {
                 scope.InjectUnsetProperties(monoBehaviour, parameters);
             }
         }
-
-        private static ILifetimeScope CreateScopeForGameObject(GameObject gameObject) => GameObjectScopes.Create(gameObject);
     }
-
-
 }
