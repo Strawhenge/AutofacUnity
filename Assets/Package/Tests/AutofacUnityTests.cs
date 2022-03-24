50cb374054cb70343c425130c5991b3dac4f7960
Matt <matt_bristow@rocketmail.com>
2022-03-24 18:26:44 +0000

    added test assembly

---
+++ Assets/Package/Tests/AutofacUnityTests.cs
@@ -0,0 +1,45 @@
+﻿using Autofac.Unity.Tests.Scripts;
+using NUnit.Framework;
+using System.Collections;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Autofac.Unity.Tests
+{
+    public class AutofacUnityTests
+    {
+        const string sceneName = "AutofacUnity";
+        const string scenePath = "Assets/Package/Tests/AutofacUnity.unity";
+
+        Player _player;
+
+        [UnitySetUp]
+        public IEnumerator LoadScene()
+        {
+            var sceneLoad = EditorSceneManager.LoadSceneAsyncInPlayMode(scenePath, new LoadSceneParameters(LoadSceneMode.Additive));
+
+            sceneLoad.completed += _ =>
+            {
+                _player = Object.FindObjectOfType<Player>();
+            };
+            
+            while (!sceneLoad.isDone)
+                yield return null;
+        }
+
+        [UnityTearDown]
+        public IEnumerator UnloadScene()
+        {
+            yield return SceneManager.UnloadSceneAsync(sceneName);
+        }
+
+        [UnityTest]
+        public IEnumerator PropertiesShouldBeInjected()
+        {
+            Assert.NotNull(_player.Inventory);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Package/Tests/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using Autofac.Unity.Tests.Services;
+using UnityEngine;
+
+namespace Autofac.Unity.Tests.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        public Inventory Inventory { get; set; }
+    }
+}
