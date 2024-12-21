d87149175282b4466d42b6d14ae674575a4afa83
Matt Bristow <strawhengestudios@gmail.com>
2024-12-21 23:10:00 +0000

    added check for root scope extension

--- Assets/Package/Runtime/Extensions.cs
+++ Assets/Package/Runtime/Extensions.cs
@@ -6,7 +6,10 @@ namespace Autofac.Unity
     public static class Extensions
     {
         public static ILifetimeScope GetScope(this IComponentContext context) =>
-           context.Resolve<ILifetimeScope>();
+            context.Resolve<ILifetimeScope>();
+
+        public static bool IsRootScope(this IComponentContext context) =>
+            context.GetScope().Tag is "root";
 
         public static GameObject GetGameObject(this IComponentContext context) =>
             context.Resolve<GameObject>();
@@ -14,10 +17,12 @@ namespace Autofac.Unity
         public static T ResolveFromGameObject<T>(this IComponentContext context) where T : Object =>
             context.GetGameObject().GetComponent<T>();
 
-        public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> RegisterTypeFromGameObject<T>(this ContainerBuilder builder) where T : Object =>
+        public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle>
+            RegisterTypeFromGameObject<T>(this ContainerBuilder builder) where T : Object =>
             builder.Register(x => x.ResolveFromGameObject<T>());
 
-        public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> RegisterTypeFromScene<T>(this ContainerBuilder builder) where T : Object =>
-           builder.Register(x => Object.FindObjectOfType<T>()).InstancePerLifetimeScope();
+        public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> RegisterTypeFromScene<T>(
+            this ContainerBuilder builder) where T : Object =>
+            builder.Register(x => Object.FindObjectOfType<T>()).InstancePerLifetimeScope();
     }
 }
