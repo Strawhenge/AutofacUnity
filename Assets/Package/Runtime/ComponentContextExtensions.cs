c06aa0ea4fd5e5572f91b5b55d83090ed0ff8b27
Strawhenge <strawhengestudios@gmail.com>
2025-05-29 07:11:47 +0100

    tidy up

---
+++ Assets/Package/Runtime/ComponentContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Autofac.Unity
+{
+    public static class ComponentContextExtensions
+    {
+        public static ILifetimeScope GetScope(this IComponentContext context) =>
+            context.Resolve<ILifetimeScope>();
+
+        public static bool IsRootScope(this IComponentContext context) =>
+            context.GetScope().Tag is "root";
+
+        public static GameObject GetGameObject(this IComponentContext context) =>
+            context.Resolve<GameObject>();
+
+        public static T ResolveFromGameObject<T>(this IComponentContext context) where T : Object =>
+            context.GetGameObject().GetComponent<T>();
+    }
+}
--- Assets/Package/Runtime/ContainerBuilderExtensions.cs
+++ Assets/Package/Runtime/ContainerBuilderExtensions.cs
@@ -3,20 +3,8 @@ using UnityEngine;
 
 namespace Autofac.Unity
 {
-    public static class Extensions
+    public static class ContainerBuilderExtensions
     {
-        public static ILifetimeScope GetScope(this IComponentContext context) =>
-            context.Resolve<ILifetimeScope>();
-
-        public static bool IsRootScope(this IComponentContext context) =>
-            context.GetScope().Tag is "root";
-
-        public static GameObject GetGameObject(this IComponentContext context) =>
-            context.Resolve<GameObject>();
-
-        public static T ResolveFromGameObject<T>(this IComponentContext context) where T : Object =>
-            context.GetGameObject().GetComponent<T>();
-
         public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle>
             RegisterTypeFromGameObject<T>(this ContainerBuilder builder) where T : Object =>
             builder.Register(x => x.ResolveFromGameObject<T>());
